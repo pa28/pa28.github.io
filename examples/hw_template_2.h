e5ecb1b2a076d923a4ab33064a3bbeab34c3c3d2
Richard Buckley <richard@hrbuckley.net>
2018-08-12 12:56:32 -0400

    Reformat to fit on web page.

--- examples/hw_template_2.h
+++ examples/hw_template_2.h
@@ -2,29 +2,35 @@ template<int _storage_bits, typename _output_policy, typename _store_policy>
 struct hw_register {
     typedef _output_policy output_policy;
     typedef _store_policy store_policy;
-    typedef typename required_base_type<required_base_size<_storage_bits>(),store_policy::optimize_speed>::base_type base_type;
+    typedef typename required_base_type<required_base_size<_storage_bits>(),
+            store_policy::optimize_speed>::base_type base_type;
     static constexpr int base_type_bits = std::numeric_limits<base_type>::digits;
     static constexpr int storage_bits = _storage_bits;
     static constexpr base_type mask = static_cast<base_type>((1 << storage_bits) - 1);
 
     hw_register() : value{0} {}
 
-    template<typename T, typename = typename std::enable_if_t<is_hw_register_v<T>>>
+    template<typename T, typename = typename std::enable_if_t<is_hw_register_v < T>>>
+
     explicit hw_register(T const &other) : value{static_cast<base_type>(other() & mask)} {}
 
-    template <typename T, typename = typename std::enable_if_t<is_hw_register_v<T>>>
+    template<typename T, typename = typename std::enable_if_t<is_hw_register_v < T>>>
+
     hw_register &operator=(T const &other) {
         static_assert(not store_policy::check_storage_size || (T::storage_bits <= storage_bits),
                       "Slice too wide for register storage.");
-        static_assert(T::base_type_bits <= base_type_bits, "Slice too wide for register base type.");
+        static_assert(T::base_type_bits <= base_type_bits,
+                      "Slice too wide for register base type.");
         value = static_cast<base_type>(other.value);
         return *this;
     }
 
-    template<typename T, typename = typename std::enable_if_t<std::is_integral_v<T>>>
+    template<typename T, typename = typename std::enable_if_t<std::is_integral_v < T>>>
+
     explicit hw_register(T init_value) : value{static_cast<base_type>(init_value & mask)} {}
 
-    template<typename T, typename = typename std::enable_if_t<std::is_integral_v<T>>>
+    template<typename T, typename = typename std::enable_if_t<std::is_integral_v < T>>>
+
     hw_register &operator=(T v) {
         value = static_cast<base_type>(v & mask);
         return *this;
@@ -34,21 +40,28 @@ struct hw_register {
         return value;
     }
 
-    template<typename T, typename = typename std::enable_if_t<is_hw_slice_spec_v<T>>>
-    auto operator[](T const &slice_spec) const {
-        static_assert(not store_policy::check_storage_size || ((T::width + T::offset) <= storage_bits),
-                      "Slice too wide for register storage.");
-        static_assert((T::width + T::offset) <= base_type_bits, "Slice too wide for register base type.");
-        typedef typename required_base_type<required_base_size<T::width + T::offset>()>::base_type new_base_type;
+    template<typename T, typename = typename std::enable_if_t<is_hw_slice_spec_v < T>>>
 
-        return hw_slice<new_base_type, T::width, T::offset>{value};
+    auto operator[](T const &slice_spec) const {
+        static_assert(not store_policy::check_storage_size ||
+                      ((T::width + T::offset) <= storage_bits),
+                      "Slice too wide for register storage.");
+        static_assert((T::width + T::offset) <= base_type_bits,
+                      "Slice too wide for register base type.");
+        typedef typename required_base_type<required_base_size<T::width + T::offset>()>::base_type
+                new_base_type;
+
+        return hw_slice < new_base_type, T::width, T::offset > {value};
     }
 
-    template<typename T, typename = typename std::enable_if_t<is_hw_slice_v<T>>>
+    template<typename T, typename = typename std::enable_if_t<is_hw_slice_v < T>>>
+
     hw_register &operator<<(T const &slice) {
-        static_assert(not store_policy::check_storage_size || ((T::width + T::offset) <= storage_bits),
+        static_assert(not store_policy::check_storage_size ||
+                      ((T::width + T::offset) <= storage_bits),
                       "Slice too wide for register storage.");
-        static_assert((T::width + T::offset) <= base_type_bits, "Slice too wide for register base type.");
+        static_assert((T::width + T::offset) <= base_type_bits,
+                      "Slice too wide for register base type.");
         value = (value & ~static_cast<base_type>(T::mask)) | (slice.value_in_place());
     }
 
@@ -60,32 +73,32 @@ struct hw_register {
                        << std::dec;
     }
 
-    template <typename T, typename = typename std::enable_if_t<is_hw_register_v<T>>>
+    template<typename T, typename = typename std::enable_if_t<is_hw_register_v < T>>>
     bool operator==(T const &other) const {
         return value == other.value;
     }
 
-    template <typename T, typename = typename std::enable_if_t<is_hw_register_v<T>>>
+    template<typename T, typename = typename std::enable_if_t<is_hw_register_v < T>>>
     bool operator!=(T const &other) const {
         return value != other.value;
     }
 
-    template <typename T, typename = typename std::enable_if_t<is_hw_register_v<T>>>
+    template<typename T, typename = typename std::enable_if_t<is_hw_register_v < T>>>
     bool operator<(T const &other) const {
         return value < other.value;
     }
 
-    template <typename T, typename = typename std::enable_if_t<is_hw_register_v<T>>>
+    template<typename T, typename = typename std::enable_if_t<is_hw_register_v < T>>>
     bool operator>(T const &other) const {
         return value > other.value;
     }
 
-    template <typename T, typename = typename std::enable_if_t<is_hw_register_v<T>>>
+    template<typename T, typename = typename std::enable_if_t<is_hw_register_v < T>>>
     bool operator<=(T const &other) const {
         return value <= other.value;
     }
 
-    template <typename T, typename = typename std::enable_if_t<is_hw_register_v<T>>>
+    template<typename T, typename = typename std::enable_if_t<is_hw_register_v < T>>>
     bool operator>=(T const &other) const {
         return value >= other.value;
     }
