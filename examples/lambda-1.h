80dc79c0c9605b519c05224f29c21fc1ebfaebf0
Richard Buckley <richard@hrbuckley.net>
2018-08-27 12:48:09 -0400

    Clean up jump targets. Add function-bind-placeholders.html

---
+++ examples/lambda-1.h
@@ -0,0 +1,8 @@
+...
+status_lights.write([this](
+status_lights_t::data_ptr_t &data_ptr
+) {
+this->
+status_lights_writer(data_ptr);
+});
+...
---
+++ examples/reader-writer.h
@@ -0,0 +1,7 @@
+std::function<
+void(std::unique_ptr<data_type_t>
+const &)>
+reader
+
+        std::function<void(std::unique_ptr < data_type_t > &)>
+writer
